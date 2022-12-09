a5b62f0f0362d1c917c12b92012093b2b2b77a50
Ravi Varma <ravi.k.varma@outlook.com>
2022-12-09 20:02:38 +0530

    Adding D6 files

---
+++ D6-60.py
@@ -0,0 +1,15 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if not right_is_clear():
+#         if front_is_clear():
+#             move()
+#         else:
+#             turn_left()
+#     else:
+#         turn_right()
+#         move()
